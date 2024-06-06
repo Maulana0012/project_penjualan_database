108b49bc8fb73d2e8e5bf779ea219d617477ab6e
Maulana0012 <waytrial03@gmail.com>
2024-06-07 01:11:16 +0800

    insert and showing table data

--- src/Main/MainFile.java
+++ src/Main/MainFile.java
@@ -16,13 +16,35 @@ public class MainFile {
      */
     public static void main(String[] args) {
         Database dbConnect = new Database();
-        dbConnect.insertValueUMKM("001UF", "Batik Boga", "Rahmadi", "Fashion", "Jalan C", "3313091704331", "AISAS");
-        // Input Table Industr
+        dbConnect.showTable("konsumen");
+//======= Input Table umkm         
+//        dbConnect.insertValueUMKM("001UF", "Batik Boga", "Rahmadi", "Fashion", "Jalan C", "3313091704331", "AISA");
+//        dbConnect.insertValueUMKM("001UM", "Gelato", "Rahmadi", "Makanan", "Jalan D", "3313091704332", "ADES");
+//        dbConnect.insertValueUMKM("001UT", "Galora TechNova", "Wayruni", "Teknologi", "Jalan A", "3313091704333", "ALTO");
+//        dbConnect.insertValueUMKM("001DG", "Desain Out", "Bima", "Desain Grafis", "Jalan S", "3313091704334", "BTEK");
+//        dbConnect.insertValueUMKM("001KO", "Course Mam", "Riski", "Kursus Online", "Jalan M", "3313091704335", "BUDI");
+
+//======= Input Tabla jual_produk
+//        dbConnect.insertValueJualProduk("001F", "001UF", "Guccpot");
+//        dbConnect.insertValueJualProduk("002M", "001UM", "Sop Naga");
+//        dbConnect.insertValueJualProduk("003T", "001UT", "Xiaher");
+//        dbConnect.insertValueJualProduk("004G", "001DG", "Baliho");
+//        dbConnect.insertValueJualProduk("005O", "001KO", "Ilmu");
+
+//======= input Table Konsumen
+//        dbConnect.insertKonsumen("001", "Mitraa", "Jalan K", "081223231101");
+//        dbConnect.insertKonsumen("002", "Rahayou", "Jalan L", "081223231102");
+//        dbConnect.insertKonsumen("003", "Wahyu", "Jalan N", "081223231103");
+//        dbConnect.insertKonsumen("004", "Samudra", "Jalan O", "081223231104");
+//        dbConnect.insertKonsumen("005", "Atlantis", "Jalan P", "081223231105");
+//        
+//        
+//======= Input Table jenis_industri
 //        dbConnect.insertValueJenisIndustri("ADES", "PT INDO FOOD");
 //        dbConnect.insertValueJenisIndustri("AISA", "PT Gucci China");
 //        dbConnect.insertValueJenisIndustri("ALTO", "PT Chanel youtube");
 //        dbConnect.insertValueJenisIndustri("BTEK", "PT INDO FOOD");
 //        dbConnect.insertValueJenisIndustri("BUDI", "PT Course Makmur");
-        
+
     }
 }
